1628779c0515fda5a502593e97968cc91b03cd60
Robert Watson <bb+lists.freebsd.perforce@cyrus.watson.org>
2005-05-30 23:15:30 +0100

    Converge on BSD style(9).
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 77746]

--- libbsm/bsm_flags.c
+++ libbsm/bsm_flags.c
@@ -31,122 +31,111 @@
 
 #include <libbsm.h>
 
-static const char *delim = ",";
+static const char	*delim = ",";
 
 /*
- * Convert the character representation of audit values
- * into the au_mask_t field
+ * Convert the character representation of audit values into the au_mask_t
+ * field.
  */
-int getauditflagsbin(char *auditstr, au_mask_t *masks)
+int
+getauditflagsbin(char *auditstr, au_mask_t *masks)
 {
 	char *tok;
 	char sel, sub;
 	struct au_class_ent *c;
 	char *last;
 
-	if((auditstr == NULL) || (masks == NULL)) {
-		return -1;
-	}
+	if ((auditstr == NULL) || (masks == NULL))
+		return (-1);
 
 	masks->am_success = 0;
 	masks->am_failure = 0;
 
 	tok = strtok_r(auditstr, delim, &last);
-	while(tok != NULL) {
-
-		/* check for the events that should not be audited */
-		if(tok[0] == '^') {
+	while (tok != NULL) {
+		/* Check for the events that should not be audited. */
+		if (tok[0] == '^') {
 			sub = 1;
 			tok++;
-		}
-		else {
+		} else
 			sub = 0;
-		}
 
-		/* check for the events to be audited for success */
-		if(tok[0] == '+') {
+		/* Check for the events to be audited for success. */
+		if (tok[0] == '+') {
 			sel = AU_PRS_SUCCESS;
 			tok++;
-		}
-		else if(tok[0] == '-') {
+		} else if (tok[0] == '-') {
 			sel = AU_PRS_FAILURE;
 			tok++;
-		}
-		else {
+		} else
 			sel = AU_PRS_BOTH;
-		}
 
-		if((c = getauclassnam(tok)) != NULL) {
-			if(sub) {
+		if ((c = getauclassnam(tok)) != NULL) {
+			if (sub)
 				SUB_FROM_MASK(masks, c->ac_class, sel);
-			}
-			else {
+			else
 				ADD_TO_MASK(masks, c->ac_class, sel);
-			}
 			free_au_class_ent(c);
-		} else {
-			return -1;
-		}
+		} else
+			return (-1);
 
-		/* Get the next class */
+		/* Get the next class. */
 		tok = strtok_r(NULL, delim, &last);
 	}
-	return 0;
+	return (0);
 }
 
 /*
- * Convert the au_mask_t fields into a string value
- * If verbose is non-zero the long flag names are used
- * else the short (2-character)flag names are used
+ * Convert the au_mask_t fields into a string value.  If verbose is non-zero
+ * the long flag names are used else the short (2-character)flag names are
+ * used.
  */
-int getauditflagschar(char *auditstr, au_mask_t *masks, int verbose)
+int
+getauditflagschar(char *auditstr, au_mask_t *masks, int verbose)
 {
 	struct au_class_ent *c;
 	char *strptr = auditstr;
 	u_char sel;
 
-	if((auditstr == NULL) || (masks == NULL)) {
-		return -1;
-	}
+	if ((auditstr == NULL) || (masks == NULL))
+		return (-1);
 
 	/*
-	 * Enumerate the class entries, check if each is selected
-	 * in either the success or failure masks
+	 * Enumerate the class entries, check if each is selected in either
+	 * the success or failure masks.
 	 */
-
-	for (setauclass(); (c = getauclassent()) != NULL; free_au_class_ent(c)) {
-
+	for (setauclass(); (c = getauclassent()) != NULL;
+	    free_au_class_ent(c)) {
 		sel = 0;
 
-		/* Dont do anything for class = no */
-		if(c->ac_class == 0) {
+		/* Dont do anything for class = no. */
+		if (c->ac_class == 0)
 			continue;
-		}
 
-		sel |= ((c->ac_class & masks->am_success) == c->ac_class) ? AU_PRS_SUCCESS : 0;
-		sel |= ((c->ac_class & masks->am_failure) == c->ac_class) ? AU_PRS_FAILURE : 0;
+		sel |= ((c->ac_class & masks->am_success) == c->ac_class) ?
+		    AU_PRS_SUCCESS : 0;
+		sel |= ((c->ac_class & masks->am_failure) == c->ac_class) ?
+		    AU_PRS_FAILURE : 0;
 
 		/*
-		 * No prefix should be attached if both
- 		 * success and failure are selected
+		 * No prefix should be attached if both success and failure
+		 * are selected.
 		 */
-		if((sel & AU_PRS_BOTH) == 0) {
-			if((sel & AU_PRS_SUCCESS) != 0) {
+		if ((sel & AU_PRS_BOTH) == 0) {
+			if ((sel & AU_PRS_SUCCESS) != 0) {
 				*strptr = '+';
 				strptr = strptr + 1;
-			}
-			else if((sel & AU_PRS_FAILURE) != 0) {
+			} else if ((sel & AU_PRS_FAILURE) != 0) {
 				*strptr = '-';
 				strptr = strptr + 1;
 			}
 		}
 
-		if(sel != 0) {
-			if(verbose) {
+		if (sel != 0) {
+			if (verbose) {
 				strcpy(strptr, c->ac_desc);
 				strptr += strlen(c->ac_desc);
-			}
-			else {
+			} else {
 				strcpy(strptr, c->ac_name);
 				strptr += strlen(c->ac_name);
 			}
@@ -155,10 +144,9 @@ int getauditflagschar(char *auditstr, au_mask_t *masks, int verbose)
 		}
 	}
 
-	/* Overwrite the last delimiter with the string terminator */
-	if(strptr != auditstr) {
+	/* Overwrite the last delimiter with the string terminator. */
+	if (strptr != auditstr)
 		*(strptr-1) = '\0';
-	}
 
-	return 0;
+	return (0);
 }
