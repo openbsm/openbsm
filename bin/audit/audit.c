a0b34e0c89b742699841c49bed6a5395adf369be
Robert Watson <bb+lists.freebsd.perforce@cyrus.watson.org>
2006-01-31 16:43:51 +0100

    Copyright canonicalization.
    $P4$
    
    Minor style tweaks.
    
    OpenBSM man page trailer.
    
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 90792]

--- bin/audit/audit.c
+++ bin/audit/audit.c
@@ -1,21 +1,22 @@
 /*
- * Copyright (c) 2005 Apple Computer, Inc. All rights reserved.
+ * Copyright (c) 2005 Apple Computer, Inc.
+ * All rights reserved.
  *
  * @APPLE_BSD_LICENSE_HEADER_START@
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1.  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer. 
+ *     notice, this list of conditions and the following disclaimer.
  * 2.  Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution. 
+ *     documentation and/or other materials provided with the distribution.
  * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
  *     its contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission. 
- * 
+ *     from this software without specific prior written permission.
+ *
  * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -26,8 +27,10 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * @APPLE_BSD_LICENSE_HEADER_END@
+ *
+ * $P4: //depot/projects/trustedbsd/openbsm/bin/audit/audit.c#2 $
  */
 /*
  * Program to trigger the audit daemon with a message that is either:
@@ -48,8 +51,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void usage()
+static void
+usage(void)
 {
+
 	(void)fprintf(stderr, "Usage: audit -n | -s | -t \n");
 	exit(-1);
 }
@@ -57,12 +62,13 @@ void usage()
 /*
  * Main routine to process command line options.
  */
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
 	char ch;
 	unsigned int trigger = 0;
 
-	if (argc != 2) 
+	if (argc != 2)
 		usage();
 
 	while ((ch = getopt(argc, argv, "nst")) != -1) {
@@ -72,7 +78,7 @@ int main(int argc, char **argv)
 			trigger = AUDIT_TRIGGER_OPEN_NEW;
 			break;
 
-		case 's':   
+		case 's':
 			trigger = AUDIT_TRIGGER_READ_FILE;
 			break;
 
