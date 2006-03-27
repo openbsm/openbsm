9ff0ad01cea9a63a18553a3d773615005a0bb123
Robert Watson <bb+lists.freebsd.perforce@cyrus.watson.org>
2006-03-27 22:15:19 +0100

    Don't mask global 'debug' variable with uninitialized local one.
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 94130]

--- bin/auditfilterd/auditfilterd.c
+++ bin/auditfilterd/auditfilterd.c
@@ -25,7 +25,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/trustedbsd/openbsm/bin/auditfilterd/auditfilterd.c#1 $
+ * $P4: //depot/projects/trustedbsd/openbsm/bin/auditfilterd/auditfilterd.c#2 $
  */
 
 #include <sys/queue.h>
@@ -169,7 +169,7 @@ main(int argc, char *argv[])
 	const char *conffile;
 	FILE *trail_fp;
 	FILE *conf_fp;
-	int ch, debug;
+	int ch;
 
 	conffile = AUDITFILTERD_CONFFILE;
 	trailfile = AUDITFILTERD_TRAILFILE;
