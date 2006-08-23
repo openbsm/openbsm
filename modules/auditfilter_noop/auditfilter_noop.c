ad7c8f61fbb3b13cbb163809f82049be17d51f96
Robert Watson <bb+lists.freebsd.perforce@cyrus.watson.org>
2006-08-23 06:51:15 +0100

    Catch up with BSM->AUDIT rename for filter API.
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 104826]

--- modules/auditfilter_noop/auditfilter_noop.c
+++ modules/auditfilter_noop/auditfilter_noop.c
@@ -25,7 +25,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/trustedbsd/openbsm/modules/auditfilter_noop/auditfilter_noop.c#2 $
+ * $P4: //depot/projects/trustedbsd/openbsm/modules/auditfilter_noop/auditfilter_noop.c#3 $
  */
 
 /*
@@ -60,7 +60,7 @@ AUDIT_FILTER_RECORD(void *instance, struct timespec *ts, int token_count,
 }
 
 void
-AUDIT_FILTER_BSMRECORD(void *instance, struct timespec *ts, u_char *data,
+AUDIT_FILTER_AUDITRECORD(void *instance, struct timespec *ts, u_char *data,
     u_int len)
 {
 
