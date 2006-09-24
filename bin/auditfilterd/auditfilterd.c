d5bbb2df0b054bcf48d334d08c3ee1a498962825
Robert Watson <bb+lists.freebsd.perforce@cyrus.watson.org>
2006-09-25 00:25:19 +0100

    Submit untested Mac OS X clock_gettime() compat code so that it's in the tree where csjp can get to it.
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 106621]

--- bin/auditfilterd/auditfilterd.c
+++ bin/auditfilterd/auditfilterd.c
@@ -25,7 +25,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/trustedbsd/openbsm/bin/auditfilterd/auditfilterd.c#9 $
+ * $P4: //depot/projects/trustedbsd/openbsm/bin/auditfilterd/auditfilterd.c#10 $
  */
 
 /*
@@ -46,6 +46,10 @@
 #include <sys/queue.h>
 #else
 #include <compat/queue.h>
+#endif
+
+#ifndef HAVE_CLOCK_GETTIME
+#include <compat/clock_gettime.h>
 #endif
 
 #include <bsm/libbsm.h>
