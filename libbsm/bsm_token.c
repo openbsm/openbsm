0aee38edc97816a19ce93b8f753b7cbb7783415e
Christian S.J. Peron <csjp@freebsd.org>
2006-11-22 15:40:25 +0100

    Instead of coverting the data to big endian on copy, do a straight memory copy. The user is expected to have the address data in network byte order already. This is consistent with how we read the machine portion of the regular termid.
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 110390]

--- libbsm/bsm_token.c
+++ libbsm/bsm_token.c
@@ -30,7 +30,7 @@
  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * $P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_token.c#55 $
+ * $P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_token.c#56 $
  */
 
 #include <sys/types.h>
@@ -1104,12 +1104,10 @@ au_to_subject32_ex(au_id_t auid, uid_t euid, gid_t egid, uid_t ruid,
 	ADD_U_INT32(dptr, sid);
 	ADD_U_INT32(dptr, tid->at_port);
 	ADD_U_INT32(dptr, tid->at_type);
-	ADD_U_INT32(dptr, tid->at_addr[0]);
-	if (tid->at_type == AU_IPv6) {
-		ADD_U_INT32(dptr, tid->at_addr[1]);
-		ADD_U_INT32(dptr, tid->at_addr[2]);
-		ADD_U_INT32(dptr, tid->at_addr[3]);
-	}
+	if (tid->at_type == AU_IPv6)
+		ADD_MEM(dptr, &tid->at_addr[0], 4 * sizeof(u_int32_t));
+	else
+		ADD_MEM(dptr, &tid->at_addr[0], sizeof(u_int32_t));
 
 	return (t);
 }
