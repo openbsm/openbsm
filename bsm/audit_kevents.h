a1dd0f9ae379c261fd8d5127091b2b00bb6aabd5
Robert Watson <bb+lists.freebsd.perforce@cyrus.watson.org>
2007-05-14 14:31:54 +0100

    Remove apple license header tags, since we don't use mechanical license management in OpenBSM.
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 119842]

--- bsm/audit_kevents.h
+++ bsm/audit_kevents.h
@@ -2,8 +2,6 @@
  * Copyright (c) 2005 Apple Computer, Inc.
  * All rights reserved.
  *
- * @APPLE_BSD_LICENSE_HEADER_START@
- *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -28,9 +26,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * @APPLE_BSD_LICENSE_HEADER_END@
- *
- * $P4: //depot/projects/trustedbsd/openbsm/bsm/audit_kevents.h#47 $
+ * $P4: //depot/projects/trustedbsd/openbsm/bsm/audit_kevents.h#48 $
  */
 
 #ifndef _BSM_AUDIT_KEVENTS_H_
