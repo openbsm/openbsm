9649b45b09ac925d5d6528ca71b9b10e53b322c4
Robert Watson <bb+lists.freebsd.perforce@cyrus.watson.org>
2008-11-12 20:25:17 +0100

    In au_assemble(), rather than failing if EPERM is returned on querying the kernel audit state, generate a token without that additional state.  This allows building audit records without privilege, such as in our regression suite.
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 152889]

--- libbsm/bsm_audit.c
+++ libbsm/bsm_audit.c
@@ -30,7 +30,7 @@
  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * $P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_audit.c#31 $
+ * $P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_audit.c#32 $
  */
 
 #include <sys/types.h>
@@ -221,7 +221,7 @@ au_assemble(au_record_t *rec, short event)
 	aia.ai_termid.at_type = AU_IPv4;
 	aia.ai_termid.at_addr[0] = INADDR_ANY;
 	if (auditon(A_GETKAUDIT, &aia, sizeof(aia)) < 0) {
-		if (errno != ENOSYS)
+		if (errno != ENOSYS && errno != EPERM)
 			return (-1);
 #endif /* HAVE_AUDIT_SYSCALLS */
 		tot_rec_size = rec->len + AUDIT_HEADER_SIZE +
