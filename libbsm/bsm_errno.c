4b1cce90642cf6b231523a87479e219fc79bdab5
Robert Watson <bb+lists.freebsd.perforce@cyrus.watson.org>
2008-12-19 13:34:41 +0100

    bsm_errno.c doesn't require endian.h since errno values on the wire are single-byte, so don't do the include magic/evil.
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 154995]

--- libbsm/bsm_errno.c
+++ libbsm/bsm_errno.c
@@ -26,26 +26,12 @@
  * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE. 
  *
- * $P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_errno.c#11 $
+ * $P4: //depot/projects/trustedbsd/openbsm/libbsm/bsm_errno.c#12 $
  */
 
 #include <sys/types.h>
 
 #include <config/config.h>
-#ifdef HAVE_SYS_ENDIAN_H
-#include <sys/endian.h>
-#else /* !HAVE_SYS_ENDIAN_H */
-#ifdef HAVE_MACHINE_ENDIAN_H
-#include <machine/endian.h>
-#else /* !HAVE_MACHINE_ENDIAN_H */
-#ifdef HAVE_ENDIAN_H
-#include <endian.h>
-#else /* !HAVE_ENDIAN_H */
-#error "No supported endian.h"
-#endif /* !HAVE_ENDIAN_H */
-#endif /* !HAVE_MACHINE_ENDIAN_H */
-#include <compat/endian.h>
-#endif /* !HAVE_SYS_ENDIAN_H */
 
 #include <bsm/audit_errno.h>
 #include <bsm/libbsm.h>
