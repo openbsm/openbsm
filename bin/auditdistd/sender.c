9d6bd166b5d26d6e797c37c49f69f4965218092f
Christian Brueffer <brueffer@freebsd.org>
2014-08-28 13:05:26 +0100

    Unlock the right lock.
    The adist_remote_lock is not held in this place, whereas the
    adist_recv_list_lock lock is and is picked up during the next iteration.
    
    Submitted by:	ed
    
    [git-p4: depot-paths = "//depot/projects/trustedbsd/openbsm/": change = 1199465]

--- bin/auditdistd/sender.c
+++ bin/auditdistd/sender.c
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $P4: //depot/projects/trustedbsd/openbsm/bin/auditdistd/sender.c#4 $
+ * $P4: //depot/projects/trustedbsd/openbsm/bin/auditdistd/sender.c#5 $
  */
 
 #include <config/config.h>
@@ -643,7 +643,7 @@ recv_thread(void *arg __unused)
 			 * we can use that.
 			 */
 			if (TAILQ_EMPTY(&adist_recv_list)) {
-				rw_unlock(&adist_remote_lock);
+				mtx_unlock(&adist_recv_list_lock);
 				continue;
 			}
 			mtx_unlock(&adist_recv_list_lock);
