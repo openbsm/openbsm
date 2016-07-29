439a6fdc910b25da2595680c5cda1ed148066c91
Robert N. M. Watson <robert.watson@cl.cam.ac.uk>
2016-07-29 20:22:41 +0000

    Add BSM audit event identifier for sctp_generic_sendmsg_iov(2).

--- sys/bsm/audit_kevents.h
+++ sys/bsm/audit_kevents.h
@@ -639,6 +639,7 @@
 #define	AUE_GETLOGINCLASS	43237	/* FreeBSD-specific. */
 #define	AUE_SETLOGINCLASS	43238	/* FreeBSD-specific. */
 #define	AUE_POSIX_FADVISE	43239	/* FreeBSD-specific. */
+#define	AUE_SCTP_GENERIC_SENDMSG_IOV	43240	/* FreeBSD-specific. */
 
 /*
  * Darwin BSM uses a number of AUE_O_* definitions, which are aliased to the
